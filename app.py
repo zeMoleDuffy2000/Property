751fe01ab1fb0b23077f3f7543ee509bd8a11d8c
Phil <poduffy@gmail.com>
2020-12-22 14:52:32 +0000

    new commit

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(_name_)
-
-@app.route("/")
-def main():
-    return "welcome to my Flask page!"
-
-if _name_ == "_main_":
-    app.run(debug=True, host="0.0.0.0", port=80)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
